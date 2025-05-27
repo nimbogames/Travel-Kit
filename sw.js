8dbfb9cb238444242841a85aadec536317773943
nimbo <nimboclay@gmail.com>
2025-05-27 19:47:27 +0530

    Update and rename sw.js to no need.txt

--- sw.js
+++
@@ -1,9 +0,0 @@
-
-self.addEventListener('install', function(event) {
-  console.log('Service worker installed.');
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
-});
