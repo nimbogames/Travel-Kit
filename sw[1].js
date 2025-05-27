f99c6ddec648056d2b0f788c2c1ca3705d8eca52
nimbo <nimboclay@gmail.com>
2025-05-27 19:23:20 +0530

    Add files via upload

---
+++ sw[1].js
@@ -0,0 +1,9 @@
+
+self.addEventListener('install', function(event) {
+  console.log('Service worker installed.');
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
